0b20c315936854c046870bac41730c0ca5e1cfa2
itspectahemo <charlieyoussef21@gmail.com>
2025-11-26 21:48:21 -0500

    Create .env file and environment variables.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from datetime import date
+#FLASK/SQLAlchemy modules
 from flask import Flask, abort, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
@@ -9,12 +9,18 @@ from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Text, ForeignKey
 from typing import List
 from functools import wraps
+# Other modules
+import os
+from dotenv import load_dotenv
+from datetime import date
 from werkzeug.security import generate_password_hash, check_password_hash
 # Import forms from the forms.py
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 
+load_dotenv()
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -48,7 +54,7 @@ def admin_only(f):
 class Base(DeclarativeBase):
     pass
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -222,4 +228,4 @@ def contact():
     return render_template("contact.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5003)
+    app.run(debug=False, port=5003)
